804a354ca3c81e40548a8546b198abed9d3d3635
Peedeedle <s1806443@glos.ac.uk>
2022-04-14 09:27:11 +0100

    Initial Script names

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////
+// File: Checkpoint.cs
+// Author: Jack Peedle
+// Date Created: 14/04/22
+// Last Edited By: Jack Peedle
+// Date Last Edited: 14/04/22
+// Brief:   
+//////////////////////////////////////////////////////////// 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
